1d1303007a29bfbca487c35ce6c1470dd10154af
xGfrost <fadelakbar528@gmail.com>
2024-08-28 15:26:27 +0700

    push 1

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, {Express, Request, Response} from 'express';
+import { PORT } from './secrets';
+import rootRouter from './routes';
+
+const app:Express = express();
+
+app.use('/api', rootRouter)
+
+app.listen(PORT, () => { 
+    console.log('app working!'); 
+});
