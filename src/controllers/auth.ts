7900c508a82a643bd8685f5a8e6ad6016019ab6e
xGfrost <fadelakbar528@gmail.com>
2024-08-28 15:54:46 +0700

    push 2

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,25 @@
 import {Request, Response} from 'express'
+import { prismaClient } from '..';
+import {hashSync} from 'bcrypt'
 
-export const login = (req: Request, res: Response) =>{
-    res.send('login works')
+export const signup = async (req: Request, res: Response) =>{
+    const {email, password, name} = req.body;
+
+    let user = await prismaClient.user.findFirst({
+        where:{
+            email
+        }
+    })
+    if (user) {
+        throw Error('user already exists!')
+    }
+    user = await prismaClient.user.create({
+        data: {
+            name,
+            email,
+            password: hashSync(password, 10)
+        }
+    })
+    res.json(user)
     
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express, {Express, Request, Response} from 'express';
 import { PORT } from './secrets';
 import rootRouter from './routes';
+import { PrismaClient } from '@prisma/client';
 
 const app:Express = express();
 
+app.use(express.json())
+
 app.use('/api', rootRouter)
+
+export const prismaClient = new PrismaClient({
+    log:['query']
+});
 
 app.listen(PORT, () => { 
     console.log('app working!'); 
