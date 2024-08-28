1b6df3bb8cc5f3b7bc9d38f1cf6ad7f4bf9c177f
xGfrost <fadelakbar528@gmail.com>
2024-08-28 22:23:04 +0700

    push 3

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,8 @@
 import {Request, Response} from 'express'
 import { prismaClient } from '..';
-import {hashSync} from 'bcrypt'
+import {hashSync, compareSync} from 'bcrypt'
+import * as jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../secrets';
 
 export const signup = async (req: Request, res: Response) =>{
     const {email, password, name} = req.body;
@@ -21,5 +23,28 @@ export const signup = async (req: Request, res: Response) =>{
         }
     })
     res.json(user)
+    
+}
+
+export const login = async (req: Request, res: Response) =>{
+    const {email, password} = req.body;
+
+    let user = await prismaClient.user.findFirst({
+        where:{
+            email
+        }
+    })
+    if (!user) {
+        throw Error('user does not exists!')
+    }
+    if (!compareSync(password, user.password)) {
+        throw Error('Incorreect password!')
+    }
+    const token = jwt.sign({
+        userId: user.id
+    },JWT_SECRET)
+
+
+    res.json({user,token})
     
 }
